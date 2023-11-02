46eeb98cbcd77b2dc2d8a091cad4fada02bf299a
= <=>
2023-11-01 22:02:55 -0600

    m

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import React from 'react';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import './App.css';
 
@@ -8,12 +9,15 @@ function App() {
 
   //SPOTIFY CONSTANTS
   const CLIENT_ID = "7a2ddff09e4a43869b24d7e78c2cd9a4"
-  const REDIRECT_URI = "https://localhost:3000"
+  const REDIRECT_URI = "http://localhost:3000"
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
   //SPOTIFY CONSTANTS
 
   const [token, setToken] = useState("")
+  const [searchKey, setSearchKey] = useState("")
+  const [artists, setArtists] = useState([])
+
 
   useEffect (() => {
     const hash = window.location.hash
@@ -22,15 +26,50 @@ function App() {
     if (!token && hash) {
       token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
       console.log(token)
+      window.location.hash = ""
+      window.localStorage.setItem("token", token)
     }
-  })
+    setToken(token)
+  }, [])
+
+  const logout = () => {
+    setToken("")
+    window.localStorage.removeItem("token")
+  }
+
+  const searchArtists = async (e) => {
+    e.preventDefault()
+    const {data} = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+        q: searchKey,
+        type: "artist"
+      }
+    })
+
+    setArtists(data.artists.items)
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Spotify React</h1>
+        {!token ? 
         <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>
         Login to Spotify! </a>
+        : <button onClick={logout}>Logout</button>}
+
+        {token ? 
+          <form onSubmit={searchArtists}>
+            <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+            <button type={"submit"}>Search</button>
+          </form>
+          : <h2>Please login!</h2>
+        }
+
+        {renderArtist}
       </header>
     </div>
   );
