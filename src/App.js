087f13b6b8c59108fd81d213bce9830ba10c0776
= <=>
2023-11-03 22:19:43 -0600

    yes

--- src/App.js
+++ src/App.js
@@ -1,97 +1,125 @@
-import logo from './logo.svg';
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import Grid from './grid';
 
-import './App.css';
+const CLIENT_ID = 'your_spotify_client_id'; // Replace with your Spotify client ID
+const REDIRECT_URI = 'http://localhost:3000'; // Replace with your redirect URI
+const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
+const RESPONSE_TYPE = 'token';
+
+const OPENAI_API_KEY = 'your_openai_api_key'; // Replace with your OpenAI API key
+const OPENAI_API_ENDPOINT = 'https://api.openai.com/v1/engines/davinci/completions'; // Adjust the endpoint as needed
 
 function App() {
+  const [token, setToken] = useState(null);
+  const [artists, setArtists] = useState([]);
+  const [prompt, setPrompt] = useState('');
 
-  //SPOTIFY CONSTANTS
-  const CLIENT_ID = "7a2ddff09e4a43869b24d7e78c2cd9a4"
-  const REDIRECT_URI = "http://localhost:3000"
-  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-  const RESPONSE_TYPE = "token"
-  //SPOTIFY CONSTANTS
+  useEffect(() => {
+    // Check if a Spotify access token is in the URL hash on page load
+    const hashParams = new URLSearchParams(window.location.hash.substring(1));
+    const accessToken = hashParams.get('access_token');
 
-  const [token, setToken] = useState("")
-  const [searchKey, setSearchKey] = useState("")
-  const [artists, setArtists] = useState([])
-
-
-  useEffect (() => {
-    const hash = window.location.hash
-    let token = window.localStorage.getItem("token")
-
-    if (!token && hash) {
-      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
-      console.log(token)
-      window.location.hash = ""
-      window.localStorage.setItem("token", token)
-    }
-    setToken(token)
-  }, [])
-
-  const logout = () => {
-    setToken("")
-    window.localStorage.removeItem("token")
-  }
-
-  const searchArtists = async (e) => {
-    e.preventDefault()
-    const {data} = await axios.get("https://api.spotify.com/v1/search", {
-      headers: {
-        Authorization: `Bearer ${token}`
-      },
-      params: {
-        q: searchKey,
-        type: "artist"
+    if (accessToken) {
+      // Store the access token in local storage for later use
+      localStorage.setItem('spotifyAccessToken', accessToken);
+      setToken(accessToken);
+    } else {
+      // Check if an access token is stored in local storage
+      const storedToken = localStorage.getItem('spotifyAccessToken');
+      if (storedToken) {
+        setToken(storedToken);
       }
-    })
+    }
+  }, []);
 
-    setArtists(data.artists.items)
-  }
+  const handleLogin = () => {
+    // Redirect the user to Spotify login with the required scopes
+    const scopes = 'user-top-read';
+    window.location.href = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${scopes}&response_type=${RESPONSE_TYPE}`;
+  };
 
-  const renderArtists = () => {
-  return (
-  <Grid>
-  {artists.map((artist) => (
-    <div key={artist.id}>
-      {artist.images.length ? (
-        <img width={"50%"} src={artist.images[0].url} alt={artist.name}/>
-      ) : (
-        <div>No Image</div>
-      )}
-      {artist.name}
-    </div>
-  ))}
-  </Grid>
-  );
-};
+  const handleLogout = () => {
+    // Clear the token from state and local storage
+    setToken(null);
+    localStorage.removeItem('spotifyAccessToken');
+  };
 
+  const getTopArtists = async () => {
+    try {
+      if (!token) {
+        // Handle authentication error or prompt the user to log in
+        return;
+      }
+
+      const response = await axios.get('https://api.spotify.com/v1/me/top/artists', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        params: {
+          time_range: 'long_term',
+          limit: 10,
+        },
+      });
+      setArtists(response.data.items);
+    } catch (error) {
+      console.error('Error getting top artists:', error);
+    }
+  };
+
+  const callOpenAIAPI = async () => {
+    try {
+      const response = await axios.post(
+        OPENAI_API_ENDPOINT,
+        {
+          model: 'text-davinci-003', // Specify the model
+          prompt: prompt,
+        },
+        {
+          headers: {
+            'Authorization': `Bearer ${OPENAI_API_KEY}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      console.log(response.data.choices[0].text.trim());
+    } catch (error) {
+      console.error('Error calling OpenAI API:', error);
+    }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Grid:</h1>
-        {Grid()}
-        <h1>End of Grid</h1>
         <h1>Spotify React</h1>
-        {!token ? 
-        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>
-        Login to Spotify! </a>
-        : <button onClick={logout}>Logout</button>}
+        {token ? (
+          <>
+            <button onClick={handleLogout}>Logout</button>
+            <button onClick={getTopArtists}>Get Top Artists</button>
+          </>
+        ) : (
+          <button onClick={handleLogin}>Login to Spotify</button>
+        )}
 
-        {token ? 
-          <form onSubmit={searchArtists}>
-            <input type="text" onChange={e => setSearchKey(e.target.value)}/>
-            <button type={"submit"}>Search</button>
-          </form>
-          : <h2>Please login!</h2>
-        }
-        
-        {renderArtists()}
+        {artists.length > 0 && (
+          <div>
+            <h2>Your Top Artists:</h2>
+            <ul>
+              {artists.map((artist) => (
+                <li key={artist.id}>{artist.name}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+
+        <div>
+          <textarea
+            onChange={(e) => setPrompt(e.target.value)}
+            placeholder="Enter your OpenAI prompt here"
+            cols={50}
+            rows={10}
+          />
+          <button onClick={callOpenAIAPI}>Call OpenAI API</button>
+        </div>
       </header>
     </div>
   );
--- src/grid.js
+++ src/grid.js
@@ -1,41 +1,37 @@
 import React from 'react';
 
-const Grid = () => {
-    const gridContainerStyle = {
-        display: 'grid',
-        gridTemplateColumns: 'repeat(4, 1fr)',
-        gap: '10px',
-    };
+const Grid = ({ artists }) => {
+  const gridContainerStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(4, 1fr)',
+    gap: '10px',
+  };
 
-    const gridItemStyle = {
-        border: '2px solid #1DB954', // Spotify green color
-        borderRadius: '10px', // Rounded corners
-        padding: '10px', // Add padding for content inside the boxes
-    };
+  const gridItemStyle = {
+    border: '2px solid #1DB954', // Spotify green color
+    borderRadius: '10px', // Rounded corners
+    padding: '10px', // Add padding for content inside the boxes
+  };
 
-    return (
-        <div>
-            <p style={{ fontSize: 12 }}>Grid with Dynamic Data</p>
-            <div style={gridContainerStyle}>
-                <div style={gridItemStyle}>Example</div>
-                <div style={gridItemStyle}>Example</div>
-                <div style={gridItemStyle}>Example</div>
-                <div style={gridItemStyle}>Example</div>
-                <div style={gridItemStyle}>Example</div>
-                <div style={gridItemStyle}>Example</div>
-                <div style={gridItemStyle}>Example</div>
-                <div style={gridItemStyle}>Example</div>
-                <div style={gridItemStyle}>Example</div>
-                <div style={gridItemStyle}>Example</div>
-                <div style={gridItemStyle}>Example</div>
-                <div style={gridItemStyle}>Example</div>
-                <div style={gridItemStyle}>Example</div>
-                <div style={gridItemStyle}>Example</div>
-                <div style={gridItemStyle}>Example</div>
-                <div style={gridItemStyle}>Example</div>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <p style={{ fontSize: 12 }}>Grid with Dynamic Data</p>
+      <div style={gridContainerStyle}>
+        {artists.map((artist) => (
+          <div key={artist.id} style={gridItemStyle}>
+            {artist.images.length ? (
+              <div>
+                <img width={"100%"} src={artist.images[0].url} alt={artist.name} />
+                {artist.name}
+              </div>
+            ) : (
+              <div>No Image</div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Grid;
